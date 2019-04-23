d8726104bcc28a79ad6c505d4fe4d2cf158689c7
Mu-An Chiou <me@muanchiou.com>
2019-04-23 15:01:39 -0400

    Ensure undoing a line with OL works without selection

--- index.js
+++ index.js
@@ -484,13 +484,27 @@ function orderedList(textarea: HTMLTextAreaElement): SelectionRange {
   let selectionEnd
   let selectionStart
   let text = textarea.value.slice(textarea.selectionStart, textarea.selectionEnd)
+  let textToUnstyle = text
   let lines = text.split('\n')
-
-  const undoStyling = lines.every(line => orderedListRegex.test(line))
+  let startOfLine, endOfLine
+  if (noInitialSelection) {
+    const linesBefore = textarea.value.slice(0, textarea.selectionStart).split(/\n/)
+    startOfLine = textarea.selectionStart - linesBefore[linesBefore.length - 1].length
+    endOfLine = wordSelectionEnd(textarea.value, textarea.selectionStart, true)
+    textToUnstyle = textarea.value.slice(startOfLine, endOfLine)
+  }
+  const linesToUnstyle = textToUnstyle.split('\n')
+  const undoStyling = linesToUnstyle.every(line => orderedListRegex.test(line))
 
   if (undoStyling) {
-    lines = lines.map(line => line.replace(orderedListRegex, ''))
+    lines = linesToUnstyle.map(line => line.replace(orderedListRegex, ''))
     text = lines.join('\n')
+    if (noInitialSelection && startOfLine && endOfLine) {
+      const lengthDiff = linesToUnstyle[0].length - lines[0].length
+      selectionStart = selectionEnd = textarea.selectionStart - lengthDiff
+      textarea.selectionStart = startOfLine
+      textarea.selectionEnd = endOfLine
+    }
   } else {
     lines = (function() {
       let i
