762cdf03efecd6f98a39424871550766cf4569f7
Kristján Oddsson <koddsson@gmail.com>
2019-09-25 17:14:24 +0100

    check to see if level is less than one
    Co-Authored-By: Mu-An 慕安 <me@muanchiou.com>

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ class MarkdownHeaderButtonElement extends MarkdownButtonElement {
     super()
 
     const level = parseInt(this.getAttribute('level') || 3, 10)
-    if (level > 6) {
+    if (level < 1 || level > 6) {
       return
     }
 
